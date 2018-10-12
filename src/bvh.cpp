779c91c2fd818fbffd715c46f1f65d474e2a7d1e
s7 <799671203@qq.com>
2018-10-12 11:20:17 +0800

    bvh sah work

--- src/bvh.cpp
+++ src/bvh.cpp
@@ -52,19 +52,86 @@ BVHBuildNode* recursiveBuild(std::vector<BVHPrimitiveInfo>& primitiveInfo,
             // we partition them into equally sized subsets
             // ------------------------------------------
 
-            //if (nPrimitives <= 4) {
+            if (nPrimitives <= 4) {
                 //partitation primitives into equally sized subsets
 
                 std::nth_element(&primitiveInfo[start], &primitiveInfo[static_cast<int>(mid)], &primitiveInfo[end - 1] + 1,
                     [dim](const BVHPrimitiveInfo& a, const BVHPrimitiveInfo& b) {
                     return a.centroid[dim] < b.centroid[dim];
                 });
-            //}
-        }
-        node->InitInterior(dim,
-            recursiveBuild(primitiveInfo, start, static_cast<int>(mid), totalNodes, orderedPrims, primitives),
-            recursiveBuild(primitiveInfo, static_cast<int>(mid), end, totalNodes, orderedPrims, primitives));
+            } else {
+                //sah
+                const int nBuckets = 12;
+                BucketInfo buckets[nBuckets];
 
+                for (int i = start; i < end; ++i) {
+                    int b = nBuckets * centroidBounds.Offset(primitiveInfo[i].centroid)[dim];
+                    if (b == nBuckets) {
+                        b = nBuckets - 1;
+                    }
+                    buckets[b].count++;
+                    buckets[b].bounds = Union(buckets[b].bounds, primitiveInfo[i].bounds);
+                }
+
+                //calculate cost
+                float cost[nBuckets - 1];
+                for (int i = 0; i < nBuckets - 1; ++i) {
+                    Bounds3f b0;
+                    Bounds3f b1;
+                    int count0 = 0;
+                    int count1 = 0;
+                    for (int j = 0; j <= i; ++j) {
+                        b0 = Union(b0, buckets[j].bounds);
+                        count0 += buckets[j].count;
+                    }
+                    for (int k = i + 1; k < nBuckets; ++k) {
+                        b1 = Union(b1, buckets[k].bounds);
+                        count1 += buckets[k].count;
+                    }
+                    cost[i] = 1.0f + (b0.SurfaceArea() * count0 + b1.SurfaceArea() * count1) / bounds.SurfaceArea();
+                }
+
+                //find min cost
+                float minCost = FLT_MAX;
+                int minID = -1;
+                for (int i = 0; i < nBuckets - 1; ++i) {
+                    if (cost[i] < minCost) {
+                        minCost = cost[i];
+                        minID = i;
+                    }
+                }
+
+                float leafCost = static_cast<float>(nPrimitives);
+                //partition according to sah min cost
+                if (nPrimitives > g_maxPrimsInNode || leafCost > minCost) {
+                    BVHPrimitiveInfo *pmid = std::partition(
+                        &primitiveInfo[start],
+                        &primitiveInfo[end - 1] + 1,
+                        [=](const BVHPrimitiveInfo& pi) {
+                        int b = nBuckets * centroidBounds.Offset(pi.centroid)[dim];
+                        if (b == nBuckets) {
+                            b = nBuckets - 1;
+                        }
+                        return b <= minID;
+                    });
+                    mid = pmid - &primitiveInfo[0];
+                } else {
+                    //leaf node;
+                    int firstOffest = orderedPrims.size();
+                    for (int i = start; i < end; ++i) {
+                        int primitiveIdx = primitiveInfo[i].primitiveNumber;
+                        orderedPrims.push_back(primitives[primitiveIdx]);
+                    }
+                    node->InitLeaf(firstOffest, nPrimitives, bounds);
+                    return node;
+                }
+
+            }
+
+            node->InitInterior(dim,
+                recursiveBuild(primitiveInfo, start, static_cast<int>(mid), totalNodes, orderedPrims, primitives),
+                recursiveBuild(primitiveInfo, static_cast<int>(mid), end, totalNodes, orderedPrims, primitives));
+        }
     }
     return node;
 }
