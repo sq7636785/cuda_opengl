7f300d86572abced3d3e37dc47cb1b8c44a4c240
s7 <799671203@qq.com>
2018-10-13 23:36:33 +0800

    fix bugs and update project set

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int                 height;
 
 int main() {
     //const char* sceneFile = "cornellBox_threeSphere.txt";
-    const char* sceneFile = "cornell_bunny.txt";
+    const char* sceneFile = "../scenes/cornell_bunny.txt";
     scene = new Scene(sceneFile);
 
     iteration = 0;
